3f91f8a185313af0cbaae49c095c0b3b773dfea3
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-03-20 12:09:16 +0800

    added profile model and modified recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
@@ -13,6 +17,14 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return self.name
@@ -32,4 +44,3 @@ class RecipeIngredient(models.Model):
         on_delete=models.CASCADE,
         related_name="ingredients"
         )
-    
