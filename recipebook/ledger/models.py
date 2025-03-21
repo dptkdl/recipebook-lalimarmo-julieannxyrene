8c15bd6966ef3421c4240b2f553b819742d07083
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-03-21 11:21:21 +0800

    modified profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -4,8 +4,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
     bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
