674566165cc44f480c98dfd5b0ecbdea4e79eeb2
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-04-07 20:00:09 +0800

    created forms.py

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -1,24 +1,22 @@
 from django import forms
-from .models import Recipe, RecipeImage
+from .models import Recipe, RecipeImage, RecipeIngredient, Ingredient
 
-# Form for creating a Recipe
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ['name', 'author']  # Only include fields you want users to fill out
+        fields = ["name"]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Recipe Name'})
-        self.fields['author'].widget.attrs.update({'class': 'form-control'})  # Author will be set automatically
+class RecipeIngredientForm(forms.ModelForm):
+    class Meta:
+        model = RecipeIngredient
+        fields = ["recipe", "ingredient", "quantity"]
 
-# Form for uploading a Recipe Image
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = "__all__"
+
 class RecipeImageForm(forms.ModelForm):
     class Meta:
         model = RecipeImage
-        fields = ['image', 'description']  # Allow users to add an image and description
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['image'].widget.attrs.update({'class': 'form-control'})
-        self.fields['description'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Image Description'})
+        fields = "__all__"
