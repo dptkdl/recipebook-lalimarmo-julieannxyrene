6e9871cedb3432608e66dc4f9c7eca0f57942379
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-04-07 21:37:37 +0800

    modified views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -19,25 +19,24 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     context_object_name = "recipe"
     model = Recipe
     template_name = "recipe.html"
-    extra = {"images":RecipeImage.objects.all}
+    extra_context = {"images":RecipeImage.objects.all}
 
 class RecipeCreateView(LoginRequiredMixin, CreateView):
     '''
     creates view fpr new recipe form
     '''
     model = Recipe
-    form_class = RecipeForm
     template_name = "recipe_form.html"
+    form_class = RecipeForm
     success_url = "/recipe/add/recipeingredient/"
 
     def form_valid(self, form):
-        # Assign the currently logged-in user as the author
         form.instance.author = self.request.user.profile
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super(RecipeCreateView, self).get_context_data(**kwargs)
-        context['recipe_form']=context['form']
+        context['recipe_form'] = context['form']
         return context
 
 # Upload Recipe Image
@@ -45,13 +44,13 @@ class RecipeIngredientCreateView(LoginRequiredMixin, CreateView):
     '''
     creates view for recipe ingredient of recipe detail form
     '''
-    form_class = RecipeIngredientForm
-    template_name = 'recipeingredient_form.html'
     model = RecipeIngredient
+    template_name = "recipeingredient_form.html"
+    form_class = RecipeIngredientForm
     success_url = reverse_lazy('ledger:recipes-list')
 
     def get_context_data(self, **kwargs):
-        context = super(RecipeIngredientCreateView).get_context_data(**kwargs)
+        context = super(RecipeIngredientCreateView, self).get_context_data(**kwargs)
         context['recipeingredient_form'] = context['form']
         return context
 
@@ -59,10 +58,10 @@ class IngredientCreateView(LoginRequiredMixin, CreateView):
     '''
     creates view for new ingredient form
     '''
-    form_class = IngredientForm
-    template_name = 'ingredient_form.html'
     model = Ingredient
-    success_url = 'recipe/add/recipeingredient'
+    template_name = "ingredient_form.html"
+    form_class = IngredientForm
+    success_url = "/recipe/add/recipeingredient/"
 
     def get_context_data(self, **kwargs):
         context = super(IngredientCreateView, self).get_context_data(**kwargs)
@@ -74,12 +73,12 @@ class RecipeImageCreateView(LoginRequiredMixin, CreateView):
     creates view for new recipe image form
     '''
     model = RecipeImage
-    template_name = 'recipeimage_form.html'
+    template_name = "recipeimage_form.html"
     form_class = RecipeImageForm
 
     def get_success_url(self):
-        return reverse_lazy('legder:recipe-detail', kwargs={ 'pk':self.object.recipe.pk})
-    
+        return reverse_lazy('ledger:recipe-detail', kwargs={ 'pk': self.object.recipe.pk })
+
     def get_context_data(self, **kwargs):
         context = super(RecipeImageCreateView, self).get_context_data(**kwargs)
         context['recipeimage_form'] = context['form']
