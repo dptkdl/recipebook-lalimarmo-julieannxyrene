8e780cff89417c874fa8ad6ca5a71dce86d85673
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-04-07 21:12:04 +0800

    modifies settings and urls

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -20,11 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('ledger.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('ledger.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, 
+                      document_root=settings.MEDIA_ROOT)
