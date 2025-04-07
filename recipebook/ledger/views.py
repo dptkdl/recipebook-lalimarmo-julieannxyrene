fcff642a22144c9a9a8a2f57afe2202c875679cf
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-04-07 20:22:37 +0800

    added views

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,50 +1,86 @@
-from django.shortcuts import redirect
-from django.urls import reverse
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
-from django.views.generic.edit import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Recipe, RecipeImage
-from .forms import RecipeForm, RecipeImageForm
+from .models import Recipe, RecipeImage, RecipeIngredient, Ingredient
+from .forms import RecipeForm, RecipeImageForm, RecipeIngredientForm, IngredientForm
 
-# List of Recipes
 class RecipeListView(LoginRequiredMixin, ListView):
+    '''
+    creates list view for recipe list
+    '''
     context_object_name = "recipes"
-    model = Recipe
+    queryset = Recipe.objects.all()
     template_name = "recipes.html"
 
-# Recipe Detail
 class RecipeDetailView(LoginRequiredMixin, DetailView):
+    '''
+    creates detailed view for the recipe
+    '''
     context_object_name = "recipe"
     model = Recipe
     template_name = "recipe.html"
+    extra = {"images":RecipeImage.objects.all}
 
-# Create a Recipe
 class RecipeCreateView(LoginRequiredMixin, CreateView):
+    '''
+    creates view fpr new recipe form
+    '''
     model = Recipe
     form_class = RecipeForm
     template_name = "recipe_form.html"
+    success_url = "/recipe/add/recipeingredient/"
 
     def form_valid(self, form):
         # Assign the currently logged-in user as the author
         form.instance.author = self.request.user.profile
         return super().form_valid(form)
 
-    def get_success_url(self):
-        return reverse('ledger:recipe-detail', args=[self.object.pk])
+    def get_context_data(self, **kwargs):
+        context = super(RecipeCreateView, self).get_context_data(**kwargs)
+        context['recipe_form']=context['form']
+        return context
 
 # Upload Recipe Image
-class RecipeImageUploadView(LoginRequiredMixin, FormView):
-    form_class = RecipeImageForm
-    template_name = 'recipe_image_form.html'
-
-    def form_valid(self, form):
-        # Associate the image with the specific recipe
-        recipe = Recipe.objects.get(pk=self.kwargs['pk'])
-        form.instance.recipe = recipe
-        form.save()
-        return redirect('ledger:recipe-detail', pk=recipe.pk)
+class RecipeIngredientCreateView(LoginRequiredMixin, CreateView):
+    '''
+    creates view for recipe ingredient of recipe detail form
+    '''
+    form_class = RecipeIngredientForm
+    template_name = 'recipeingredient_form.html'
+    model = RecipeIngredient
+    success_url = reverse_lazy('ledger:recipes-list')
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['recipe'] = Recipe.objects.get(pk=self.kwargs['pk'])
+        context = super(RecipeIngredientCreateView).get_context_data(**kwargs)
+        context['recipeingredient_form'] = context['form']
+        return context
+
+class IngredientCreateView(LoginRequiredMixin, CreateView):
+    '''
+    creates view for new ingredient form
+    '''
+    form_class = IngredientForm
+    template_name = 'ingredient_form.html'
+    model = Ingredient
+    success_url = 'recipe/add/recipeingredient'
+
+    def get_context_data(self, **kwargs):
+        context = super(IngredientCreateView, self).get_context_data(**kwargs)
+        context['ingredient_form'] = context['form']
+        return context
+    
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    '''
+    creates view for new recipe image form
+    '''
+    model = RecipeImage
+    template_name = 'recipeimage_form.html'
+    form_class = RecipeImageForm
+
+    def get_success_url(self):
+        return reverse_lazy('legder:recipe-detail', kwargs={ 'pk':self.object.recipe.pk})
+    
+    def get_context_data(self, **kwargs):
+        context = super(RecipeImageCreateView, self).get_context_data(**kwargs)
+        context['recipeimage_form'] = context['form']
         return context
