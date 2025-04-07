2b993b126ee63c59c8ecadafff30de9651101d98
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-04-07 19:39:46 +0800

    added admin panel for recipe image

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Ingredient, Recipe, RecipeIngredient, Profile
+from .models import Ingredient, Recipe, RecipeIngredient, Profile, RecipeImage
 
 class IngredientAdmin(admin.ModelAdmin):
     '''
@@ -10,19 +10,25 @@ class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
     list_display = ('name', )
 
-class RecipeAdmin(admin.ModelAdmin):
-    '''
-    creates the admin panel for the Recipe model
-    '''
-    model = Recipe
-    list_display = ('name', )
-
 class RecipeIngredientAdmin(admin.ModelAdmin):
     '''
     created the admin panel for the RecipeIngredient model
     '''
     model = RecipeIngredient
     list_display = ('quantity', 'ingredient', 'recipe', )
+
+class RecipeImageInLine(admin.TabularInline):
+    model = RecipeImage
+    extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+    '''
+    creates the admin panel for the Recipe model
+    '''
+    model = Recipe
+    list_display = ('name', 'author', 'created_on', 'updated_on')
+    inlines = [RecipeImageInLine]
+
 
 class ProfileInLine(admin.StackedInline):
     '''
