fd7661551df241073c98eb417ab94330bf9c987b
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2025-04-07 20:24:31 +0800

    modified admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -10,27 +10,29 @@ class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
     list_display = ('name', )
 
-class RecipeIngredientAdmin(admin.ModelAdmin):
+class RecipeIngredientInline(admin.TabularInline):
     '''
-    created the admin panel for the RecipeIngredient model
+    creates the admin panel for the RecipeIngredient model
     '''
     model = RecipeIngredient
     list_display = ('quantity', 'ingredient', 'recipe', )
 
-class RecipeImageInLine(admin.TabularInline):
+class RecipeImageInline(admin.StackedInline):
+    '''
+    creates admin panel for RecipeImage model
+    '''
     model = RecipeImage
-    extra = 1
 
 class RecipeAdmin(admin.ModelAdmin):
     '''
     creates the admin panel for the Recipe model
     '''
     model = Recipe
-    list_display = ('name', 'author', 'created_on', 'updated_on')
-    inlines = [RecipeImageInLine]
+    list_display = ('name', )
+    inlines = [RecipeImageInline, RecipeIngredientInline]
 
 
-class ProfileInLine(admin.StackedInline):
+class ProfileInline(admin.StackedInline):
     '''
     makes sure that the Profile model is with the User model
     '''
@@ -41,10 +43,9 @@ class UserAdmin(BaseUserAdmin):
     '''
     adds Profile model to the User panel
     '''
-    inlines = [ProfileInLine,]
+    inlines = [ProfileInline,]
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
